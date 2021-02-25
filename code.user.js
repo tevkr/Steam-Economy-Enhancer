8250fb2e5e65b5c254003013e61e68cb1a83d6e2
nom <vlad.nomerov@yandex.ru>
2021-02-25 21:24:29 +0300

    branch1 change ver to 6.8.5

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/Nuklon
 // @author      Nuklon
 // @license     MIT
-// @version     6.8.4
+// @version     6.8.5
 // @description Enhances the Steam Inventory and Steam Market.
 // @include     *://steamcommunity.com/id/*/inventory*
 // @include     *://steamcommunity.com/profiles/*/inventory*
